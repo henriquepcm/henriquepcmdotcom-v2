0d1cf36faeb6106c7d05192e4c2f12afc6ff65e0
Henrique Pochmann <henriquepcm@gmail.com>
2024-12-11 16:29:38 -0300

    test loadingabove fold

--- src/components/Animations/HomeAnimation.tsx
+++ src/components/Animations/HomeAnimation.tsx
@@ -1,81 +1,8 @@
-import { useSpring, useInView, animated } from "@react-spring/web";
-
 export default function HomeAnimation() {
-     const [refHomeAnimation, inViewHomeAnimation] = useInView({
-          rootMargin: "-50% 0%",
-     });
-
-     const styleLaptop = useSpring({
-          from: {
-               opacity: 0,
-               filter: "blur(5rem)",
-               transform: "scale(0.5)",
-               x: -200,
-          },
-          to: {
-               opacity: inViewHomeAnimation ? 1 : 0,
-               filter: inViewHomeAnimation ? "blur(0)" : "blur(5rem)",
-               transform: inViewHomeAnimation ? "scale(1)" : "scale(0.5)",
-               x: inViewHomeAnimation ? 0 : -200,
-          },
-
-          delay: 0,
-          config: { friction: 50 },
-     });
-
-     const stylePhone = useSpring({
-          from: {
-               opacity: 0,
-               filter: "blur(5rem)",
-               transform: "scale(0.5)",
-               x: 200,
-          },
-          to: {
-               opacity: inViewHomeAnimation ? 1 : 0,
-               filter: inViewHomeAnimation ? "blur(0)" : "blur(5rem)",
-               transform: inViewHomeAnimation ? "scale(1)" : "scale(0.5)",
-               x: inViewHomeAnimation ? 0 : 200,
-          },
-          delay: 500,
-          config: { friction: 50 },
-     });
-
-     const styleH1 = useSpring({
-          from: {
-               opacity: 0,
-               y: -10,
-          },
-          to: {
-               opacity: inViewHomeAnimation ? 1 : 0,
-               y: inViewHomeAnimation ? 0 : -10,
-          },
-          delay: 1000,
-          config: { friction: 50 },
-     });
-
-     const styleRole = useSpring({
-          from: {
-               opacity: 0,
-               y: -10,
-          },
-          to: {
-               opacity: inViewHomeAnimation ? 1 : 0,
-               y: inViewHomeAnimation ? 0 : -10,
-          },
-          delay: 1500,
-          config: { friction: 50 },
-     });
-
      return (
           <div className="flex flex-col flex-grow justify-center items-center -translate-y-10 lg:-translate-y-0 relative">
-               <animated.div
-                    ref={refHomeAnimation}
-                    className="flex flex-row items-center translate-y-5"
-               >
-                    <animated.div
-                         style={stylePhone}
-                         className="hidden lg:flex flex-col"
-                    >
+               <div className="flex flex-row items-center translate-y-5">
+                    <div className="hidden lg:flex flex-col">
                          <picture>
                               <source
                                    srcSet="./img/phone-kiwi-sideways-retina.avif"
@@ -101,9 +28,9 @@ export default function HomeAnimation() {
                                    className="w-[7rem] -translate-y-12 translate-x-12"
                               />
                          </picture>
-                    </animated.div>
+                    </div>
 
-                    <animated.div style={styleLaptop} className="flex flex-col">
+                    <div className="flex flex-col">
                          <picture>
                               <source
                                    srcSet="./img/laptop-retina.avif"
@@ -129,20 +56,15 @@ export default function HomeAnimation() {
                                    className="w-[19rem] -translate-y-20 -translate-x-4 z-10"
                               />
                          </picture>
-                    </animated.div>
-               </animated.div>
+                    </div>
+               </div>
                <div className="flex flex-col items-center absolute z-50 mt-[26rem] w-[18.8rem] lg:w-full">
                     <div className="flex justify-center items-center text-center leading-[3rem]">
-                         <animated.h1 style={styleH1}>
-                              Henrique Pochmann
-                         </animated.h1>
+                         <h1>Henrique Pochmann</h1>
                     </div>
-                    <animated.div
-                         style={styleRole}
-                         className="flex justify-center uppercase text-white text-center leading-[1.5rem] tracking-[0.44rem]"
-                    >
+                    <div className="flex justify-center uppercase text-white text-center leading-[1.5rem] tracking-[0.44rem]">
                          Front-end Developer & UI/UX Designer
-                    </animated.div>
+                    </div>
                </div>
           </div>
      );
