817f6e1ce089951524e2b2cd807e6714bb0d3f34
Henrique Pochmann <henriquepcm@gmail.com>
2024-12-12 09:38:12 -0300

    test compression

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { compression } from "vite-plugin-compression2";
 
 // https://vite.dev/config/
 export default defineConfig({
-     plugins: [react()],
+     plugins: [react(), compression()],
      server: {
           host: "192.168.2.248",
           port: 5173,
