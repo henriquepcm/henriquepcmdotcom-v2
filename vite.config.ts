e5bc92588216f61661c1251e6a648632a7153a7d
Henrique Pochmann <henriquepcm@gmail.com>
2024-12-12 10:09:33 -0300

    remove prior compression test

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { compression } from "vite-plugin-compression2";
 
 // https://vite.dev/config/
 export default defineConfig({
-     plugins: [react(), compression()],
+     plugins: [react()],
      server: {
           host: "192.168.2.248",
           port: 5173,
